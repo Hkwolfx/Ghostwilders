2cf4cfbdeb6d98c519a01b4c9dd8d44806e97f9d
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-10-02 15:53:37 +0200

    buttons open and hide now active

--- script.js
+++ script.js
@@ -70,3 +70,17 @@ window.onload = changeArt;
 
 /* Hover */
 
+
+/* Footer effect  */
+const hideButton = document.getElementById('hide');
+const openButton = document.getElementById('boutonContactFooter');
+
+
+openButton.addEventListener('click' , () => {
+  document.getElementById('contactInfosHome').style.display = 'flex';
+});
+
+hideButton.addEventListener('click' , () => {
+  document.getElementById('contactInfosHome').style.display = 'none';
+});
+
